a9043e2d5247a3842a535c19010f13f561869b31
XavierShayne35 <149250298+XavierShayne35@users.noreply.github.com>
2023-12-09 11:05:51 +0100

    chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 import random
+import os
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -19,11 +20,38 @@ async def ciao(ctx):
 async def helpme(ctx):
     await ctx.send(f'Ecco la lista dei comandi:(metti sempre davanti il prefisso $)')
     await ctx.send(f'ciao, che ti permette di essere salutato')
+    await ctx.send(f'gen_pass, che scritto inseme al numero di caratteri di cui vuoi che la password sia, genera una password casuale')
+    await ctx.send(f'gen_emoji,invia un emoji casuale')
+    await ctx.send(f'flip_coin, che ti permette di lanciare una moneta')
     await ctx.send(f'heh, che va scritto assieme a quante volte lo si vuole ripetere')
     await ctx.send(f'add, che va scritto assieme ai numeri che si vuole sommare')
     await ctx.send(f'roll, che va scritto prima quanti dadi vogliamo lanciare poi d e poi di quanto deve essere il dado. Es: 1d6')
     await ctx.send(f'choose, che va scritto insieme ad altre parole ed il bot ne sceglierà una a caso')
     await ctx.send(f'repeat, che va scritto prima il numero di quante volte si vuole ripetere una parola e poi la parola')
+    await ctx.send(f'gen_meme, invia un meme causale')
+
+@bot.command()
+async def gen_pass(pass_length):
+    elements = "abcdefghijklmnopqrstuvwxyz123456789+-/*!&$#?=@<>"
+    password = ""
+
+    for i in range(pass_length):
+        password += random.choice(elements)
+
+    return password
+
+@bot.command()
+async def gen_emodji():
+    emodji = ["\U0001f600", "\U0001f642", "\U0001F606", "\U0001F923"]
+    return random.choice(emodji)
+
+@bot.command()
+async def flip_coin():
+    flip = random.randint(0, 2)
+    if flip == 0:
+        return "HEADS"
+    else:
+        return "TAILS"
 
 @bot.command()
 async def heh(ctx, count_heh = 5):
@@ -57,6 +85,28 @@ async def repeat(ctx, times: int, content='repeating...'):
     for i in range(times):
         await ctx.send(content)
 
+@bot.command()
+async def meme(ctx):
+    with open('images/1.png', 'rb') as f:
+        # Memorizziamo il file della libreria di Discord convertito in questa variabile!
+        picture = discord.File(f)
+   # Possiamo quindi inviare questo file come parametro!
+    await ctx.send(file=picture)
+
+import os
+
+@bot.command()
+async def gen_meme(ctx, name=None):
+    folder = "images"
+    memes = os.listdir(folder)
+    
+    if name is None or name not in memes:
+        name = random.choice(memes)
+
+    with open(f'{folder}/{name}', 'rb') as f:
+        picture = discord.File(f)
+        await ctx.send(file=picture)
+    
 @bot.command()
 async def joined(ctx, member: discord.Member):
     """Says when a member joined."""
